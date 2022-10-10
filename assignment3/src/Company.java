cbe331df0d3316b15f96436ab5849a43485756d5
agrimasingh <agrima2503@gmail.com>
2022-10-10 14:32:07 +0200

    Company Class

--- assignment3/src/Company.java
+++ assignment3/src/Company.java
@@ -1,2 +1,30 @@
-public class Company {
+import java.util.ArrayList;
+public class Company
+{
+    private ArrayList<Employee> listOfEmployees;
+
+    public Company ()
+    {
+        this.listOfEmployees= new ArrayList<>();
+    }
+
+    public void registerEmployee (String id, String name, double grossSalary)
+    {
+        Employee employee = new Employee (id, name, grossSalary);
+        this.listOfEmployees.add(employee);
+    }
+
+    public Employee findEmployee (String id)
+    {
+        for (int i=0; i<listOfEmployees.size(); i++)
+        {
+            Employee currentEmployee = listOfEmployees.get(i);
+            if(id.equals(currentEmployee.getID()))
+            {
+                return currentEmployee;
+            }
+        }
+        return null;
+    }
 }
+
